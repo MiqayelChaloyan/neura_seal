513ac93d5c8c53f38bb2c0d5038ffd6866c1d133
MiqayelChaloyan <miqochaloyan@gmail.com>
2025-07-13 05:09:27 +0400

    feat: industries section

---
+++ src/pages/Technology/IndustriesSection/index.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import './styles.css';
+
+import { Instagram, Linkedin, Youtube  } from 'lucide-react';
+
+export default function IndustriesSection() {
+  return (
+    <section className="industries-section">
+      <div className="crisis-section-outer">
+        <div className="industries-section-title">
+          <h1>Industry's We Serve</h1>
+        </div>
+        <div className="industries-section-border-wrapper">
+          <div className="industries-section-container">
+            <div className="industries-section-main">
+              <div className="industry-card hover-highlight">
+                <Instagram width={50} height={50} className="industry-icon" />
+                <h3>LUXURY GOODS & FASHION</h3>
+                <p>Protecting high-value brands from counterfeit infiltration.</p>
+              </div>
+              <div className="industry-card hover-highlight">
+                <Linkedin width={50} height={50} className="industry-icon" />
+                <h3>PHARMACEUTICALS & HEALTHCARE</h3>
+                <p>Ensuring drug authenticity and preventing life-threatening fraud.</p>
+              </div>
+              <div className="industry-card hover-highlight">
+                <Youtube width={50} height={50} className="industry-icon" />
+                <h3>FINANCIAL INSTITUTIONS</h3>
+                <p>Securing financial documents such as cheques, banknotes, and certificates against forgery</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
