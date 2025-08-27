a83a353128deaab6dd7937c78186739934e9f008
MiqayelChaloyan <miqochaloyan@gmail.com>
2025-08-28 03:11:50 +0400

    feat: slide & ui/ux

--- src/constants/hosts.jsx
+++ src/constants/hosts.jsx
@@ -1,12 +1,11 @@
 const Hosts = {
-    INSTAGRAM: 'https://www.instagram.com/neura_seal',
-    FACEBOOK: 'https://www.facebook.com/neura.seal',
-    YOUTUBE: 'https://www.youtube.com/@neura_seal',
-    LINKEDIN: 'https://www.linkedin.com/company/neura-seal',
-    TWITTER: 'https://twitter.com/neura_seal',
-    ADDRESS: 'Al-Duhail-North Road, P.O.Box: 5479',
+    // INSTAGRAM: 'https://www.instagram.com/neura_seal',
+    // FACEBOOK: 'https://www.facebook.com/neura.seal',
+    // YOUTUBE: 'https://www.youtube.com/@neura_seal',
+    // LINKEDIN: 'https://www.linkedin.com/company/neura-seal',
+    // TWITTER: 'https://twitter.com/neura_seal',
+    ADDRESS: 'Riyadh, Saudi Arabia',
     EMAIL: 'info@neura.com',
-    PHONE: '1-234 567-8910',
 };
 
 export {
