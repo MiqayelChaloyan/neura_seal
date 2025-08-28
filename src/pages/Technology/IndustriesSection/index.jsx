0d489aa9000e9ae29f7030b2c5186a83bbabf3d7
MiqayelChaloyan <miqochaloyan@gmail.com>
2025-08-28 16:01:18 +0400

    fix: slide

--- src/pages/Technology/IndustriesSection/index.jsx
+++ src/pages/Technology/IndustriesSection/index.jsx
@@ -23,30 +23,13 @@ const settings = {
   arrows: false,
   responsive: [
     {
-      breakpoint: 1440,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        arrows: false,
-        dots: false,
-      }
-    },
-    {
-      breakpoint: 1200,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        arrows: false,
-        dots: false,
-      }
-    },
-    {
       breakpoint: 1024,
       settings: {
         slidesToShow: 2,
         slidesToScroll: 1,
         arrows: false,
         dots: false,
+        autoplay: true,
       }
     },
     {
